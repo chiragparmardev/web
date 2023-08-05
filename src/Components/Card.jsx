afebfb487f48d49495aca68029656d28bcea4d0d
chiragparmaratzen <chiragp.zenzzen@gmail.com>
2023-08-05 14:52:42 +0530

    done

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,27 +3,36 @@ import "./Style/Card.css";
 
 export default function Card({ image, title }) {
   return (
-    <div className="col-md-3" style={{ marginBottom: "20px" }}>
-      <div className="card" style={{ backgroundColor: "#22222260" }}>
-        <div
-          className="card-body row"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+    <div className="col-md-3 " style={{ marginBottom: "20px" }}>
+      <div
+        id="cardhover"
+        className="card "
+        style={{ backgroundColor: "#22222260", cursor: "pointer" }}
+      >
+        <a
+          href="https://github.com/chiragparmardev"
+          style={{ textDecoration: "none" }}
         >
-          <div className="col-4">
-            <div className="img">
-              <img src={image} alt="img" style={{ height: "60px" }} />
+          <div
+            className="card-body row"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div className="col-4">
+              <div className="img">
+                <img src={image} alt="img" style={{ height: "60px" }} />
+              </div>
+            </div>
+            <div className="col-8">
+              <h5 className="card-title" style={{ color: "white" }}>
+                {title}
+              </h5>
             </div>
           </div>
-          <div className="col-8">
-            <h5 className="card-title" style={{ color: "white" }}>
-              {title}
-            </h5>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   );
